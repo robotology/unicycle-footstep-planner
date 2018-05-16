b749ef4d66630139bf885ed04554c26ac524acbe
Stefano <stefano.dafarra@gmail.com>
2018-05-16 11:34:35 +0200

    Using only the velocity as parameter to createPhaseTimings.
    cc @GiulioRomualdi

--- include/FootPrintsInterpolator.h
+++ include/FootPrintsInterpolator.h
@@ -68,7 +68,7 @@ class FeetInterpolator {
     std::vector<double> m_CoMHeightTrajectory, m_CoMHeightVelocity, m_CoMHeightAcceleration;
 
     bool orderSteps();
-    bool createPhasesTimings(const InitialState &weightInLeftAtMergePoint);
+    bool createPhasesTimings(const double velocityAtMergePoint);
     void fillFeetStandingPeriodsVectors();
     void fillLeftFixedVector();
     bool interpolateFoot(const std::vector<StepPhase> &stepPhase, const FootPrint &foot, std::vector<iDynTree::Transform> &output);
