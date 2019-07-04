7b4c6f2a84bfdc1eca3eb4d76a4f57b3077b3b31
Giulio Romualdi <giulio.romualdi@gmail.com>
2019-07-04 10:35:47 +0200

    remove useless function

--- src/FeetGenerator.cpp
+++ src/FeetGenerator.cpp
@@ -8,25 +8,3 @@
 #include <FeetGenerator.h>
 FeetGenerator::~FeetGenerator()
 { }
-
-iDynTree::Matrix3x3 FeetGenerator::RPYRightTrivializedSecondDerivative(const double /*roll*/, const double pitch, const double yaw, const double rollDot, const double pitchDot, const double /*yawDot*/)
-    {
-        iDynTree::Matrix3x3 map;
-
-        double sp = std::sin(pitch);
-        double cp = std::cos(pitch);
-        double sy = std::sin(yaw);
-        double cy = std::cos(yaw);
-
-        map(0, 0) = 0.0;
-        map(1, 0) = 0.0;
-        map(2, 0) = 0.0;
-        map(0, 1) = -sp * cy * rollDot;
-        map(1, 1) = -sy * sp * rollDot;
-        map(2, 1) = -cp * rollDot;
-        map(0, 2) = -(cp * sy * rollDot + cy * pitchDot);
-        map(1, 2) = cy * cp * rollDot - sy * pitchDot;
-        map(2, 2) = 0.0;
-
-        return map;
-    }
